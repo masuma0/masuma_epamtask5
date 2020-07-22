0ef575d2798626e4d9c7ad36848f543089d6d9ba
masuma0 <60343674+masuma0@users.noreply.github.com>
2020-07-22 13:39:35 +0530

    Add files via upload

---
+++ DriverLogger.java
@@ -0,0 +1,11 @@
+package epamtask5;
+
+class DisplayLogger extends CalculateSimpleAndCompoundIntrest {
+	public static void main(String args[]) { 
+        DisplayLogger invoke = new DisplayLogger();
+        	invoke.inputDetails();
+		
+	}
+
+}
+
